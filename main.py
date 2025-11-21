6571aeeda009d8e476fbe7e7ef9d85b36da841fc
Munnoi <midhunbinoy123@gmail.com>
2025-11-21 11:30:52 +0530

    21-11-25

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from problems import hello, sum_of_N
+from problems import hello, sum_of_N, reverse_string
 
 def main():
     # hello.hello()
-    sum_of_N.sum_of_N(10)
+    # sum_of_N.sum_of_N(10)
+    reverse_string.reverse_string("Hello")
 
 if __name__ == '__main__':
     main()
---
+++ problems/reverse_string.py
@@ -0,0 +1,2 @@
+def reverse_string(s):
+    print(s[::-1])
